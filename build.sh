c1b757243e91cecb11f071f660b7fa556879c25d
Noah Fahlgren <noahfahlgren@gmail.com>
2024-06-12 12:13:23 -0500

    Add temp files

--- build.sh
+++ build.sh
@@ -5,3 +5,7 @@ ln -s /usr/share/ganglia /var/www/datascience/ganglia
 mkdir /var/www/datascience/datasets
 ln -s /data/datascience/data/fahlgren_et_al_2015_bellwether.tar.gz /var/www/datascience/datasets/fahlgren_et_al_2015_bellwether.tar.gz
 ln -s /data/datascience/data/fahlgren_et_al_2015_bellwether_jpeg.tar.gz /var/www/datascience/datasets/fahlgren_et_al_2015_bellwether_jpeg.tar.gz
+
+# Temporary files
+ln -s /shares/bioinformatics/nfahlgren/projects/mrcnn/mask_rcnn_leaves_0060.h5 /var/www/datascience/mask_rcnn_leaves_0060.h5
+ln -s /home/nfahlgren/download/data.zip /var/www/datascience/data.zip
