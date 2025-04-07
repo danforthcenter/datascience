1f633a98ae44eeb8e8e9fc34a7b78e8b48f16f15
Noah Fahlgren <noahfahlgren@gmail.com>
2025-04-06 22:39:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdocs build --site-dir /var/www/datascience --clean
 mkdir /var/www/datascience/datasets
 ln -s /data/datascience/data/fahlgren_et_al_2015_bellwether.tar.gz /var/www/datascience/datasets/fahlgren_et_al_2015_bellwether.tar.gz
 ln -s /data/datascience/data/fahlgren_et_al_2015_bellwether_jpeg.tar.gz /var/www/datascience/datasets/fahlgren_et_al_2015_bellwether_jpeg.tar.gz
-ln -s /data/datascience/data/ddpsc_network_2024.html /var/www/datascience/ddpsc_network_2024.html
+ln -s /data/datascience/data/ddpsc_network_2024.html /var/www/datascience/ddpsc_network_2025.html
 ln -s /data/bart/data/bart_sorghum_nitrogen_image_data.tar.gz /var/www/datascience/datasets/bart_sorghum_nitrogen_image_data.tar.gz
 ln -s /data/bart/data/bart_nitrogen_data_and_scripts.tar.gz /var/www/datascience/datasets/bart_nitrogen_data_and_scripts.tar.gz
 ln -s /data/bart/data/bart_homography_images.zip /var/www/datascience/datasets/bart_homography_images.zip
