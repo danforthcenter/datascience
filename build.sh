f80f8d76d7f6517fef7c528f41871a7ff6fa1e73
Noah Fahlgren <noahfahlgren@gmail.com>
2025-06-07 12:00:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,3 +18,4 @@ ln -s /data/datascience/data/workshops/multiple-plant-analysis-tutorial.zip /var
 ln -s /data/datascience/data/workshops/hyperspectral-data-tutorial.zip /var/www/datascience/workshops/hyperspectral-data-tutorial.zip
 ln -s /data/datascience/data/workshops/naive-bayes-classifier-workshop.zip /var/www/datascience/workshops/naive-bayes-classifier-workshop.zip
 ln -s /data/datascience/data/workshops/bean-phenotyping-lab.zip /var/www/datascience/workshops/bean-phenotyping-lab.zip
+ln -s /data/datascience/data/workshops/stats-tests-r-workshops.zip /var/www/datascience/workshops/stats-tests-r-workshops.zip
